ca0345bb4faa64586ca1dc49fb3d92d82a1e61ff
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2017-02-01 16:44:40 +0100

    DEVENV: ta bort tutorial-kod

--- src/main/kotlin/se/inera/intyg/srs/Application.kt
+++ src/main/kotlin/se/inera/intyg/srs/Application.kt
@@ -4,12 +4,9 @@ import org.apache.cxf.Bus
 import org.apache.cxf.jaxws.EndpointImpl
 import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import se.inera.intyg.srs.customer.Customer
-import se.inera.intyg.srs.customer.CustomerRepository
 import se.inera.intyg.srs.service.GetSRSInformationResponderImpl
 import javax.xml.ws.Endpoint
 
@@ -19,35 +16,6 @@ class Application {
 
     @Autowired
     lateinit var bus: Bus
-
-    @Bean
-    fun init(repository: CustomerRepository) = CommandLineRunner {
-        repository.save(Customer("Glenn", "Johansson"))
-        repository.save(Customer("Glenn", "Andersson"))
-        repository.save(Customer("Ada", "Johansson"))
-        repository.save(Customer("Glenn", "Nilsson"))
-        repository.save(Customer("Ada", "Svensson"))
-
-        log.info("Customers found with findAll():")
-        log.info("-------------------------------")
-        for (customer in repository.findAll()) {
-            log.info(customer.toString())
-        }
-        log.info("")
-
-        val customer = repository.findOne(1L)
-        log.info("Customer found with findOne(1L):")
-        log.info("--------------------------------")
-        log.info(customer.toString())
-        log.info("")
-
-        log.info("Customer found with findByLastName('Johansson'):")
-        log.info("--------------------------------------------")
-        for (Johansson in repository.findByLastNameIgnoreCase("Johansson")) {
-            log.info(Johansson.toString())
-        }
-        log.info("")
-    }
 
     @Bean
     fun endpoint(): Endpoint {
--- src/test/kotlin/se/inera/intyg/srs/ApplicationTests.kt
+++ src/test/kotlin/se/inera/intyg/srs/ApplicationTests.kt
@@ -1,6 +1,5 @@
 package se.inera.intyg.srs
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,9 +15,7 @@ class ApplicationTests {
     lateinit var restTemplate: TestRestTemplate
 
     @Test
-    fun findAll() {
-        val content = """[{"firstName":"Glenn","lastName":"Johansson","id":1},{"firstName":"Glenn","lastName":"Andersson","id":2},{"firstName":"Ada","lastName":"Johansson","id":3},{"firstName":"Glenn","lastName":"Nilsson","id":4},{"firstName":"Ada","lastName":"Svensson","id":5}]"""
-        assertEquals(content, restTemplate.getForEntity("/customers", String::class.java).body)
+    fun dummyTest() {
+        
     }
-
 }
