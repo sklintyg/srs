81a3d7ad142d0e3761e9361522c9a1942bd1d9f5
Erik Forsgren <erik.forsgren@r2m.se>
2017-02-20 10:01:58 +0100

    DEVENV: Tog bort onödig kommentar

--- src/test/kotlin/se/inera/intyg/srs/integrationtest/util/RestAssuredUtils.kt
+++ src/test/kotlin/se/inera/intyg/srs/integrationtest/util/RestAssuredUtils.kt
@@ -2,9 +2,6 @@ package se.inera.intyg.srs.integrationtest.util
 
 import com.jayway.restassured.specification.RequestSpecification
 
-/**
- * Created by stillor on 2/17/17.
- */
 fun RequestSpecification.When(): RequestSpecification {
     return this.`when`()
 }
