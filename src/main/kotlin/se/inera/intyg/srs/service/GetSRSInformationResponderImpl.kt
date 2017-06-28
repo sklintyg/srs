668dc933908470297b601de5e91caca32b22c8aa
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2017-06-28 11:04:28 +0200

    DEVENV: några smärre schema-uppstädningar för srs

--- src/main/kotlin/se/inera/intyg/srs/service/GetSRSInformationResponderImpl.kt
+++ src/main/kotlin/se/inera/intyg/srs/service/GetSRSInformationResponderImpl.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import se.inera.intyg.clinicalprocess.healthcond.srs.getsrsinformation.v1.*
 import se.inera.intyg.srs.vo.*
+import se.riv.clinicalprocess.healthcond.certificate.types.v2.ResultCodeEnum
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
