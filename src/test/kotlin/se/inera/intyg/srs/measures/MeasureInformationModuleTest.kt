207f6ecf2f8dddb3879276a53221d53c2628a387
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2017-06-29 14:25:16 +0200

    DEVENV: fler enhetstester

--- src/test/kotlin/se/inera/intyg/srs/measures/MeasureInformationModuleTest.kt
+++ src/test/kotlin/se/inera/intyg/srs/measures/MeasureInformationModuleTest.kt
@@ -5,6 +5,9 @@ import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import se.inera.intyg.clinicalprocess.healthcond.srs.getsrsinformation.v1.Atgardsrekommendationstatus.DIAGNOSKOD_PA_HOGRE_NIVA
+import se.inera.intyg.clinicalprocess.healthcond.srs.getsrsinformation.v1.Atgardsrekommendationstatus.INFORMATION_SAKNAS
+import se.inera.intyg.clinicalprocess.healthcond.srs.getsrsinformation.v1.Atgardsrekommendationstatus.OK
 import se.inera.intyg.srs.persistence.Measure
 import se.inera.intyg.srs.persistence.MeasureRepository
 import se.inera.intyg.srs.persistence.Priority
@@ -33,10 +36,19 @@ class MeasureInformationModuleTest {
     }
 
     fun insertMeasureData() {
-        whenever(measureRepo.findByDiagnosisIdStartingWith("A12")).thenReturn(listOf(Measure(DIAGNOSIS_A12, "Depression", "1.0",
+        whenever(measureRepo.findByDiagnosisIdStartingWith(DIAGNOSIS_A12)).thenReturn(listOf(Measure(DIAGNOSIS_A12, "Depression", "1.0",
                 listOf((Priority(1, Recommendation("Softa")))))))
-        whenever(measureRepo.findByDiagnosisIdStartingWith("B12")).thenReturn(listOf(Measure(DIAGNOSIS_B12, "Benbrott", "1.0",
+        whenever(measureRepo.findByDiagnosisIdStartingWith(DIAGNOSIS_B12)).thenReturn(listOf(Measure(DIAGNOSIS_B12, "Benbrott", "1.0",
                 listOf((Priority(1, Recommendation("Hoppa på ett ben")))))))
+    }
+
+    @Test
+    fun nonExistingDiagnosisIsReportedAndTheInputDiagnosisIsReusedInOutput() {
+        val nonExisting = "Z123"
+        val person: Person = Person("1212121212", 35, Sex.MAN, Extent.HELT_NEDSATT, listOf(Diagnosis(nonExisting)))
+        val result = module.getInfo(listOf(person))
+        assertEquals(nonExisting, result.get(person)!!.rekommendation.get(0).diagnos.code)
+        assertEquals(INFORMATION_SAKNAS, result.get(person)!!.rekommendation.get(0).atgardsrekommendationstatus)
     }
 
     @Test
@@ -44,10 +56,19 @@ class MeasureInformationModuleTest {
         val person: Person = Person("1212121212", 35, Sex.MAN, Extent.HELT_NEDSATT, listOf(Diagnosis(DIAGNOSIS_A1234)))
         val result = module.getInfo(listOf(person))
         assertEquals(DIAGNOSIS_A12, result.get(person)!!.rekommendation.get(0).diagnos.code)
+        assertEquals(DIAGNOSKOD_PA_HOGRE_NIVA, result.get(person)!!.rekommendation.get(0).atgardsrekommendationstatus)
     }
 
     @Test
     fun measureShouldBeReturnedForEachMatchingDiagnosis() {
-
+        val person: Person = Person("1212121212", 35, Sex.MAN, Extent.HELT_NEDSATT, listOf(Diagnosis(DIAGNOSIS_A12), Diagnosis(DIAGNOSIS_B12)))
+        val result = module.getInfo(listOf(person))
+        assertEquals(2, result.get(person)!!.rekommendation.size)
+        assertEquals(DIAGNOSIS_A12, result.get(person)!!.rekommendation.get(0).diagnos.code)
+        assertEquals(OK, result.get(person)!!.rekommendation.get(0).atgardsrekommendationstatus)
+        assertEquals(DIAGNOSIS_B12, result.get(person)!!.rekommendation.get(1).diagnos.code)
+        assertEquals(OK, result.get(person)!!.rekommendation.get(1).atgardsrekommendationstatus)
     }
+
+
 }
