e679353e3370ec7dbfbaa2d88c939bbb3feed3d7
Erik Forsgren <erik.forsgren@r2m.se>
2017-09-13 12:18:26 +0200

    BUILDENV: Mer db

--- src/main/kotlin/se/inera/intyg/srs/persistence/Recommendation.kt
+++ src/main/kotlin/se/inera/intyg/srs/persistence/Recommendation.kt
@@ -2,10 +2,12 @@ package se.inera.intyg.srs.persistence
 
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 class Recommendation(@Id
                      val id: Long,
+                     @Column(name = "recommendation_text", columnDefinition = "clob")
                      val recommendationText: String) {
 
     override fun toString() =
