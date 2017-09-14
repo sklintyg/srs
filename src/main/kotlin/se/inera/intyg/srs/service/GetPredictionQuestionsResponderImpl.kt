2347c0b419953d298f063a75e517ec62b3fbacec
Karl Johan Andreasson <kalle.kaan@gmail.com>
2017-09-14 10:36:08 +0200

    DEVENV: GetPredictionQuestions för frågor på högre nivå

--- src/main/kotlin/se/inera/intyg/srs/service/GetPredictionQuestionsResponderImpl.kt
+++ src/main/kotlin/se/inera/intyg/srs/service/GetPredictionQuestionsResponderImpl.kt
@@ -8,34 +8,58 @@ import se.inera.intyg.clinicalprocess.healthcond.srs.getpredictionquestions.v1.G
 import se.inera.intyg.clinicalprocess.healthcond.srs.getpredictionquestions.v1.Prediktionsfraga
 import se.inera.intyg.clinicalprocess.healthcond.srs.getpredictionquestions.v1.Svarsalternativ
 import se.inera.intyg.srs.persistence.DiagnosisRepository
+import se.inera.intyg.srs.persistence.PredictionDiagnosis
 import java.math.BigInteger
+import java.util.*
 
 @Service
 @SchemaValidation(type = SchemaValidation.SchemaValidationType.BOTH)
 class GetPredictionQuestionsResponderImpl(val diagnosisRepo: DiagnosisRepository) : GetPredictionQuestionsResponderInterface {
+
+    private val MAX_ID_POSITIONS: Int = 5
+    private val MIN_ID_POSITIONS: Int = 3
+
     override fun getPredictionQuestions(request: GetPredictionQuestionsRequestType): GetPredictionQuestionsResponseType {
         val response = GetPredictionQuestionsResponseType()
-        val diagnosis = diagnosisRepo.findOneByDiagnosisId(request.diagnos.code)
-        if (diagnosis != null) {
-            diagnosis.questions.forEach { savedQuestion ->
-                val outboundQuestion = Prediktionsfraga()
-                outboundQuestion.frageid = BigInteger.valueOf(savedQuestion.id)
-                outboundQuestion.frageidSrs = savedQuestion.question.predictionId
-                outboundQuestion.fragetext = savedQuestion.question.question
-                outboundQuestion.hjalptext = savedQuestion.question.helpText
-                outboundQuestion.prioritet = BigInteger.valueOf(savedQuestion.priority.toLong())
-                savedQuestion.question.answers.forEach { savedResponse ->
-                    val outboundResponse = Svarsalternativ()
-                    outboundResponse.svarsid = BigInteger.valueOf(savedResponse.id)
-                    outboundResponse.svarsidSrs = savedResponse.predictionId
-                    outboundResponse.isDefault = savedResponse.isDefault
-                    outboundResponse.prioritet = BigInteger.valueOf(savedResponse.priority.toLong())
-                    outboundResponse.svarstext = savedResponse.answer
-                    outboundQuestion.svarsalternativ.add(outboundResponse)
-                }
-                response.prediktionsfraga.add(outboundQuestion)
+        val diagnosis = getModelForDiagnosis(request.diagnos.code) ?: return response
+
+        diagnosis.questions.forEach { savedQuestion ->
+            val outboundQuestion = Prediktionsfraga()
+            outboundQuestion.frageid = BigInteger.valueOf(savedQuestion.id)
+            outboundQuestion.frageidSrs = savedQuestion.question.predictionId
+            outboundQuestion.fragetext = savedQuestion.question.question
+            outboundQuestion.hjalptext = savedQuestion.question.helpText
+            outboundQuestion.prioritet = BigInteger.valueOf(savedQuestion.priority.toLong())
+            savedQuestion.question.answers.forEach { savedResponse ->
+                val outboundResponse = Svarsalternativ()
+                outboundResponse.svarsid = BigInteger.valueOf(savedResponse.id)
+                outboundResponse.svarsidSrs = savedResponse.predictionId
+                outboundResponse.isDefault = savedResponse.isDefault
+                outboundResponse.prioritet = BigInteger.valueOf(savedResponse.priority.toLong())
+                outboundResponse.svarstext = savedResponse.answer
+                outboundQuestion.svarsalternativ.add(outboundResponse)
             }
+            response.prediktionsfraga.add(outboundQuestion)
         }
         return response
     }
+
+    private fun getModelForDiagnosis(diagnosisId: String): PredictionDiagnosis? {
+        var currentId = cleanDiagnosisCode(diagnosisId)
+
+        if (currentId.length > MAX_ID_POSITIONS) {
+            return null
+        }
+
+        while (currentId.length >= MIN_ID_POSITIONS) {
+            val diagnosis = diagnosisRepo.findOneByDiagnosisId(currentId)
+            if (diagnosis != null) {
+                return diagnosis
+            }
+            currentId = currentId.substring(0, currentId.length - 1)
+        }
+        return null
+    }
+
+    private fun cleanDiagnosisCode(diagnosisId: String): String = diagnosisId.toUpperCase(Locale.ENGLISH).replace(".", "")
 }
--- src/test/kotlin/se/inera/intyg/srs/integrationtest/predictionquestions/PredictionQuestionsIT.kt
+++ src/test/kotlin/se/inera/intyg/srs/integrationtest/predictionquestions/PredictionQuestionsIT.kt
@@ -13,7 +13,7 @@ class PredictionQuestionsIT : BaseIntegrationTest() {
     private val SOAP_ROOT = "Envelope.Body.GetPredictionQuestionsResponse"
 
     @Test
-    fun testExistingImageShouldBeReturnedAndNonExistingShouldYieldErrorMessage() {
+    fun testGetPredictionQuestions() {
         addDiagnosis(DiagnosisRequest("M24", 0.54,
                 listOf(PredictionQuestion("Stressad?", "abc", "Hjälptext",
                         listOf(PredictionResponse("Lite kanske", "def", true),
@@ -53,5 +53,45 @@ class PredictionQuestionsIT : BaseIntegrationTest() {
                 .body("$SOAP_ROOT.prediktionsfraga[1].svarsalternativ[1].default", equalTo("true"))
     }
 
+    @Test
+    fun testGetPredictionQuestionsForHigherDiagnosis() {
+        addDiagnosis(DiagnosisRequest("M24", 0.54,
+                listOf(PredictionQuestion("Stressad?", "abc", "Hjälptext",
+                        listOf(PredictionResponse("Lite kanske", "def", true),
+                                PredictionResponse("Bara på fredagar", "ghi", false))
+                ), PredictionQuestion("Chill?", "jkl", "Hjälp mig då",
+                        listOf(PredictionResponse("Lätt mannen", "mno", false),
+                                PredictionResponse("Bara på måndagar", "pqr", true))
+                ))
+        ))
+
+        RestAssured.given()
+                .contentType(ContentType.XML)
+                .body(getClasspathResourceAsString("predictionquestions/getPredictionQuestionsRequest.xml")
+                        .replace("diagnosis_placeholder", "M2400"))
+                .whenever()
+                .post("/services/predictionquestions")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body("$SOAP_ROOT.prediktionsfraga[0].frageid-srs", equalTo("abc"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].fragetext", equalTo("Stressad?"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].hjalptext", equalTo("Hjälptext"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].svarsalternativ[0].svarsid-srs", equalTo("def"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].svarsalternativ[0].svarstext", equalTo("Lite kanske"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].svarsalternativ[0].default", equalTo("true"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].svarsalternativ[1].svarsid-srs", equalTo("ghi"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].svarsalternativ[1].svarstext", equalTo("Bara på fredagar"))
+                .body("$SOAP_ROOT.prediktionsfraga[0].svarsalternativ[1].default", equalTo("false"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].frageid-srs", equalTo("jkl"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].fragetext", equalTo("Chill?"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].hjalptext", equalTo("Hjälp mig då"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].svarsalternativ[0].svarsid-srs", equalTo("mno"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].svarsalternativ[0].svarstext", equalTo("Lätt mannen"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].svarsalternativ[0].default", equalTo("false"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].svarsalternativ[1].svarsid-srs", equalTo("pqr"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].svarsalternativ[1].svarstext", equalTo("Bara på måndagar"))
+                .body("$SOAP_ROOT.prediktionsfraga[1].svarsalternativ[1].default", equalTo("true"))
+    }
 
 }
