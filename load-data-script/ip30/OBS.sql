56ee7b765d4d3db3093bfbbd483a3fe29e8402eb
Jakob Ström <jacobo.strm@gmail.com>
2017-09-28 11:25:43 +0200

    DEVENV: Lade till testdata för ip30.

---
+++ load-data-script/ip30/OBS.sql
@@ -0,0 +1,14 @@
+# Lägger till rekommendationer av typen OBS som behövs i testerna.
+
+INSERT INTO recommendation (id, type, recommendation_text) VALUES (100, "OBS", "borsta tänderna");
+INSERT INTO recommendation (id, type, recommendation_text) VALUES (101, "OBS", "inte vara uppe för länge");
+INSERT INTO recommendation (id, type, recommendation_text) VALUES (102, "OBS", "inte prata med främlingar");
+INSERT INTO recommendation (id, type, recommendation_text) VALUES (103, "OBS", "inte svara på mail från nigerianska prinsar");
+INSERT INTO recommendation (id, type, recommendation_text) VALUES (104, "OBS", "en massa bullshit som förhoppningsvis kommer få trunkeringen att visa sina tre små fina punkter, en massa bullshit som förhoppningsvis kommer få trunkeringen att visa sina tre små fina punkter");
+
+
+INSERT INTO measure_priority VALUES (100, 1, 5, 100);
+INSERT INTO measure_priority VALUES (101, 1, 5, 101);
+INSERT INTO measure_priority VALUES (102, 1, 5, 102);
+INSERT INTO measure_priority VALUES (103, 1, 4, 103);
+INSERT INTO measure_priority VALUES (104, 1, 4, 104);
