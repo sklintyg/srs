0c0bd0da385e37066be3b176807648aabe0d5dfe
Karl Johan Andreasson <kalle.kaan@gmail.com>
2017-10-10 12:35:49 +0200

    DEVENV: Förutsätter linux filer och förutsätter inte FS=","

--- load-data-script/load-data.sh
+++ load-data-script/load-data.sh
@@ -9,24 +9,25 @@ PASSWORD=$2
 DATABASE=$3
 
 DIAGNOSIS_FILE=$4
-TMP_DIAGNOSIS_FILE=/tmp/prediction_diagnosis.txt
+TMP_DIAGNOSIS_FILE=$(mktemp)
 
 QUESTION_FILE=$5
-TMP_QUESTION_FILE=/tmp/prediction_question.txt
+TMP_QUESTION_FILE=$(mktemp)
 
 RESPONSE_FILE=$6
-TMP_RESPONSE_FILE=/tmp/prediction_response.txt
+TMP_RESPONSE_FILE=$(mktemp)
 
 LINK_FILE=$7
-TMP_LINK_FILE=/tmp/prediction_priority.txt
+TMP_LINK_FILE=$(mktemp)
 
 ATGARD_FILE=$8
-TMP_MEASURE_FILE=/tmp/measure.txt
-TMP_RECOMMENDATION_FILE=/tmp/recommendation.txt
-TMP_MEASURE_PRIORITY_FILE=/tmp/measure_priority.txt
+TMP_MEASURE_FILE=$(mktemp)
+TMP_RECOMMENDATION_FILE=$(mktemp)
+TMP_MEASURE_PRIORITY_FILE=$(mktemp)
 
 mysql -u $USERNAME -p$PASSWORD $DATABASE -e "truncate table prediction_priority; truncate table prediction_response; delete from prediction_question; delete from prediction_diagnosis; truncate measure_priority; delete from recommendation; delete from measure;"
-cat $DIAGNOSIS_FILE | sed '1d' | awk 'BEGIN { FPAT = "([^,]+)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2""}' > "$TMP_DIAGNOSIS_FILE"
+cat $DIAGNOSIS_FILE | sed '1d' | awk 'BEGIN { FS=","; FPAT = "([^,]+)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2""}' > "$TMP_DIAGNOSIS_FILE"
+cat $TMP_DIAGNOSIS_FILE
 mysql -u $USERNAME -p$PASSWORD $DATABASE -e "LOAD DATA LOCAL INFILE '$TMP_DIAGNOSIS_FILE' INTO TABLE prediction_diagnosis FIELDS TERMINATED BY '|';";
 
 declare -A diagnosis_array
@@ -35,7 +36,8 @@ do
     diagnosis_array["${diagnosis}"]=${id}
 done < <(mysql -u $USERNAME -p$PASSWORD $DATABASE -se "select id, diagnosis_id from prediction_diagnosis")
 
-cat $QUESTION_FILE | sed '1d' | awk 'BEGIN { FPAT = "([^,]+)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2"|"$3"" }' > "$TMP_QUESTION_FILE"
+cat $QUESTION_FILE | sed '1d' | awk 'BEGIN { FS=","; FPAT = "([^,]*)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2"|"$4"" }' > "$TMP_QUESTION_FILE"
+cat $TMP_QUESTION_FILE
 mysql -u $USERNAME -p$PASSWORD $DATABASE -e "LOAD DATA LOCAL INFILE '$TMP_QUESTION_FILE' INTO TABLE prediction_question FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"';"
 
 declare -A prediction_array
@@ -47,17 +49,14 @@ done < <(mysql -u $USERNAME -p$PASSWORD $DATABASE -se "select id, prediction_id 
 OLDIFS=$IFS
 IFS="|"
 
-rm -f $TMP_RESPONSE_FILE
-
 while read index prediction_question answer_text prediction_answer is_default priority
 do
-    echo "$index|$answer_text|$prediction_answer|$is_default|$priority|${prediction_array["$prediction_question"]}" >> "$TMP_RESPONSE_FILE"
-done < <(cat $RESPONSE_FILE | sed '1d' | awk 'BEGIN { FPAT = "([^,]+)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2"|"$3"|"$4"|"$5"|"$6"" }')
+    echo "$index|$answer_text|$prediction_answer|$is_default|$priority|${prediction_array["$prediction_question"]}"  >> "$TMP_RESPONSE_FILE"
+done < <(cat $RESPONSE_FILE | sed '1d' | awk 'BEGIN { FS=","; FPAT = "([^,]+)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2"|"$3"|"$4"|"$5"|"$6"" }')
 
 mysql -u $USERNAME -p$PASSWORD $DATABASE -e "LOAD DATA LOCAL INFILE '$TMP_RESPONSE_FILE' INTO TABLE prediction_response FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"' (id,answer,prediction_id,@var1,priority,question_id) SET is_default = (@var1 = 'True');"
 
 INDEX=0
-rm -f $TMP_LINK_FILE
 while read diagnosis question_id priority
 do
     TMP_IFS=$IFS
@@ -72,9 +71,6 @@ done < <(cat $LINK_FILE | sed '1d' | sed 's/,/|/g')
 
 mysql -u $USERNAME -p$PASSWORD $DATABASE -e "LOAD DATA LOCAL INFILE '$TMP_LINK_FILE' INTO TABLE prediction_priority FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"';"
 
-rm -f $TMP_MEASURE_FILE
-rm -f $TMP_MEASURE_PRIORITY_FILE
-rm -f $TMP_RECOMMENDATION_FILE
 declare -A measure_diagnosis
 DIAGNOS_INDEX=1
 while read id diagnosis diagnosis_text priority atgard version
@@ -87,7 +83,7 @@ do
     fi
     echo "$id|$atgard|REK" >> "$TMP_RECOMMENDATION_FILE"
     echo "$id|$priority|${measure_diagnosis["$diagnosis"]}|$id" >> "$TMP_MEASURE_PRIORITY_FILE"
-done < <(cat $ATGARD_FILE | sed '1d' | awk 'BEGIN { FPAT = "([^,]+)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2"|"$3"|"$4"|"$5"" }')
+done < <(cat $ATGARD_FILE | sed '1d' | awk 'BEGIN { FS=","; FPAT = "([^,]+)|(\"[^\"]+\")" } {  print ""NR"|"$1"|"$2"|"$3"|"$4"|"$5"" }')
 
 mysql -u $USERNAME -p$PASSWORD $DATABASE -e "LOAD DATA LOCAL INFILE '$TMP_MEASURE_FILE' INTO TABLE measure FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"';"
 mysql -u $USERNAME -p$PASSWORD $DATABASE -e "LOAD DATA LOCAL INFILE '$TMP_RECOMMENDATION_FILE' INTO TABLE recommendation FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '\"';"
