d95f05a3c8465a88b5eb577432ffb0d60f202503
Karl Johan Andreasson <kalle.kaan@gmail.com>
2017-11-24 14:03:47 +0100

    DEVENV: Nya texter för kategoribeskrivningar

--- src/main/kotlin/se/inera/intyg/srs/util/PredictionInformationUtil.kt
+++ src/main/kotlin/se/inera/intyg/srs/util/PredictionInformationUtil.kt
@@ -4,7 +4,7 @@ import java.math.BigInteger
 
 object PredictionInformationUtil {
     val categoryDescriptions = mapOf(BigInteger.ONE to "Prediktion saknas.",
-            BigInteger.valueOf(2) to "Ingen förhöjd risk detekterad.",
-            BigInteger.valueOf(3) to "Förhöjd risk detekterad.",
-            BigInteger.valueOf(4) to "Starkt förhöjd risk detekterad.")
+            BigInteger.valueOf(2) to "Lätt förhöjd risk",
+            BigInteger.valueOf(3) to "Måttligt förhöjd risk",
+            BigInteger.valueOf(4) to "Starkt förhöjd risk")
 }
