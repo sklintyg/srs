9453cf2edeec29ecd77baff7b1c7291a1f1b415b
Erik Forsgren <erik.forsgren@r2m.se>
2018-03-07 10:15:35 +0100

    INTYG-5572: Uppdaterat load-data.sh

--- load-data-script/load-data.sh
+++ load-data-script/load-data.sh
@@ -29,7 +29,7 @@ TMP_RECOMMENDATION_FILE=$(mktemp)
 TMP_MEASURE_PRIORITY_FILE=$(mktemp)
 
 mysql -u $USERNAME -p$PASSWORD -h $MYSQL_HOST $DATABASE -e "truncate table prediction_priority; truncate table prediction_response; delete from prediction_question; delete from prediction_diagnosis; truncate measure_priority; delete from recommendation; delete from measure;"
-cat $DIAGNOSIS_FILE | sed '1d' | awk 'BEGIN { FS="|" } {  print ""NR"|"$1"|"$2""}' > "$TMP_DIAGNOSIS_FILE"
+cat $DIAGNOSIS_FILE | sed '1d' | awk 'BEGIN { FS="|" } {  print ""NR"|"$1"|"$2"|"$3"|"$4""}' > "$TMP_DIAGNOSIS_FILE"
 mysql -u $USERNAME -p$PASSWORD -h $MYSQL_HOST $DATABASE --local-infile=1 -e "LOAD DATA LOCAL INFILE '$TMP_DIAGNOSIS_FILE' INTO TABLE prediction_diagnosis FIELDS TERMINATED BY '|';";
 
 declare -A diagnosis_array
