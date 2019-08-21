7bed3232c7b05c58abf92174d3d2a7cff888a10a
Daniel Petersson <45845951+peterssond@users.noreply.github.com>
2019-08-21 16:04:49 +0200

    Feature/intygfv 11093 (#5)
    * INTYGFV-11093: Up'd schema version due to new CXF version
    
    * INTYGFV-11093: Up'd schema version due to new CXF version
    
    * INTYGFV-11093: Up'd schema version due to new CXF version

--- performanceTest/build.gradle.kts
+++ performanceTest/build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
     compile(kotlin("stdlib", kotlinVersion))
     compile(kotlin("reflect", kotlinVersion))
 
-    compile("se.inera.intyg.clinicalprocess.healthcond.srs:intyg-clinicalprocess-healthcond-srs-schemas:0.0.8")
-    compile("se.riv.itintegration.monitoring:itintegration-monitoring-schemas:1.0.0.4")
+    compile("se.inera.intyg.clinicalprocess.healthcond.srs:intyg-clinicalprocess-healthcond-srs-schemas:0.0.13")
+    compile("se.riv.itintegration.monitoring:itintegration-monitoring-schemas:1.0.0.5")
 
     // External dependencies
     implementation("org.springframework.boot:spring-boot-starter-web")
